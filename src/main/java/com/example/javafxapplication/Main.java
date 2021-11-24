b78dfe4f5a4e57e7be1a684999fcb02280eba9e4
Prince kunzwa <princekunzwa@outlook.com>
2021-11-24 11:03:39 +0000

    CSS examples initial push

---
+++ src/main/java/com/example/javafxapplication/Main.java
@@ -0,0 +1,27 @@
+package com.example.javafxapplication;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+//            setUserAgentStylesheet(STYLESHEET_CASPIAN);
+            Scene scene = new Scene(fxmlLoader.load(), 600, 275);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
