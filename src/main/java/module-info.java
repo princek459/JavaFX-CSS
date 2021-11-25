61f66bfe764dc21d9a7c59245adc6c01c4bc65b0
Prince kunzwa <princekunzwa@outlook.com>
2021-11-25 01:18:00 +0000

    CSS final push

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module com.example.javafxapplication {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
+    requires java.desktop;
+
 
 
     opens com.example.javafxapplication to javafx.fxml;
